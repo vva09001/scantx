b85eba899587c86e972762525aa621c83eb00423
The Anh <ah16111999@gmail.com>
2019-08-28 16:23:09 +0700

    create project api

---
+++ spec-project/Spec-Project/Models/ScanInputModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spec_Project.Models
+{
+    public class ScanInputModel
+    {
+        public DateTime timeScan { get; set; }
+        public string code { get; set; }
+    }
+}
---
+++ spec-project/Spec-Project/Models/UsersModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spec_Project.Models
+{
+    public class UsersModel
+    {
+        public string fullname { get; set; }
+        public string mail { get; set; }
+        public string company { get; set; }
+        public string authorization { get; set; }
+    }
+}
