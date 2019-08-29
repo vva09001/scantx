a57bae480985737352cef0f0c01eafde49aba5a5
Truong Tran <xuantruong2310@gmail.com>
2019-08-29 11:07:08 +0700

    Add Company

---
+++ backend/Spec-Project/Models/CompanyModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spec_Project.Models
+{
+    public class CompanyModel
+    {
+        public string Company { get; set; }
+
+    }
+
+}
---
+++ backend/Spec-Project/Services/CompanyService.cs
@@ -0,0 +1,35 @@
+﻿using Spec_Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spec_Project.Services
+{
+    public interface ICompanyService
+    {
+
+        CompanyModel getCompany();
+
+
+    }
+    public class CompanyService : ICompanyService
+    {
+
+
+      
+
+        public CompanyModel getCompany()
+        {
+            CompanyModel company = (new CompanyModel
+            {
+                Company = "CSBGADSSD",
+            });
+
+            return company;
+        }
+
+       
+
+    }
+}
