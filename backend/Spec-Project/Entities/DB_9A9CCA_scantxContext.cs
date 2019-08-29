ba0284855d8b61c8317cc6299c8fbaf09d6aea60
The Anh <ah16111999@gmail.com>
2019-08-29 12:51:41 +0700

    update

---
+++ backend/Spec-Project/Entities/DB_9A9CCA_scantxContext.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Spec_Project.Entities
+{
+    public partial class DB_9A9CCA_scantxContext : DbContext
+    {
+        public DB_9A9CCA_scantxContext()
+        {
+        }
+
+        public DB_9A9CCA_scantxContext(DbContextOptions<DB_9A9CCA_scantxContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TblUsers> TblUsers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=sql5045.site4now.net;Integrated Security=False;Database=DB_9A9CCA_scantx;User ID=DB_9A9CCA_scantx_admin;Password=Vbn*34295;MultipleActiveResultSets=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.6-servicing-10079");
+
+            modelBuilder.Entity<TblUsers>(entity =>
+            {
+                entity.ToTable("tblUsers");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Authorization)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Company).HasMaxLength(150);
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(150)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.FamilyName).HasMaxLength(150);
+
+                entity.Property(e => e.GivenName).HasMaxLength(150);
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasMaxLength(150)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.TypeOfAccount)
+                    .IsRequired()
+                    .HasMaxLength(150)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.UserName)
+                    .IsRequired()
+                    .HasMaxLength(150);
+            });
+        }
+    }
+}
---
+++ backend/Spec-Project/Entities/TblUsers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Spec_Project.Entities
+{
+    public partial class TblUsers
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string GivenName { get; set; }
+        public string FamilyName { get; set; }
+        public string TypeOfAccount { get; set; }
+        public string Authorization { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public bool? ContactByEmail { get; set; }
+        public bool? EncryptionActive { get; set; }
+        public string Company { get; set; }
+    }
+}
