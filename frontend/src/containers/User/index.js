d6d9384dc571ec3ed1210b217b018140bc527f3d
Duong The Pham <duongphamhn97@gmail.com>
2019-09-05 11:51:13 +0700

    Fix User page

--- frontend/src/containers/User/index.js
+++ frontend/src/containers/User/index.js
@@ -1,47 +1,59 @@
 import React, { Component } from "react";
-import { connect } from 'react-redux';
-import { CircularProgress } from 'components/uielements/progress';
-import LayoutWrapper from "../components/utility/layoutWrapper";
-import Papersheet from "../components/utility/papersheet";
-import { FullColumn } from "../components/utility/rowColumn";
+import { connect } from "react-redux";
+import { CircularProgress } from "components/uielements/progress";
+import LayoutWrapper from "components/utility/layoutWrapper";
+import Papersheet from "components/utility/papersheet";
+import { FullColumn } from "components/utility/rowColumn";
 import Button from "components/uielements/button/index.js";
 import Grid from "components/uielements/grid";
 import Table from "components/uielements/table";
-import Checkbox from 'components/uielements/checkbox';
-import {
-  TableBody,
-  TableCell,
-  TableRow,
-} from 'components/uielements/table';
-import { userActions } from 'redux/actions';
-import _ from 'lodash';
-import "../styles/style.css";
+import Checkbox from "components/uielements/checkbox";
+import { TableBody, TableCell, TableRow } from "components/uielements/table";
+import { userActions } from "redux/actions";
+import _ from "lodash";
+import "styles/style.css";
+
+const role = roleId => {
+  if (roleId === "2") {
+    return "admin";
+  } else if (roleId === "3") {
+    return "user";
+  } else {
+    return "No Role";
+  }
+};
 
 class User extends Component {
   constructor(props) {
     super(props);
     this.state = {
       loading: true
-    }
+    };
   }
   componentDidMount() {
-    this.props.getUser({ userID: 1 }, this.onSuccess, this.onSuccess)
+    this.props.getUser({ userID: 1 }, this.onSuccess, this.onSuccess);
   }
   onSuccess = () => {
     this.setState({
       loading: false
-    })
-  }
+    });
+  };
   renderData = () => {
-    return _.map(this.props.users, (item, key) => (
-      <TableRow key={key}>
-        <TableCell padding="checkbox"><Checkbox /></TableCell>
-        <TableCell>{item.familyName} {item.givenName}</TableCell>
-        <TableCell>{item.email}</TableCell>
-        <TableCell>{item.userName}</TableCell>
-      </TableRow>
-    ))
-  }
+    return _.map(this.props.users, item => {
+      return (
+        <TableRow key={item.id}>
+          <TableCell padding="checkbox">
+            <Checkbox />
+          </TableCell>
+          <TableCell>
+            {item.givenName} {item.familyName}
+          </TableCell>
+          <TableCell>{item.email}</TableCell>
+          <TableCell>{role(item.roleId)}</TableCell>
+        </TableRow>
+      );
+    });
+  };
   render() {
     if (this.state.loading) {
       return <CircularProgress />;
@@ -51,9 +63,7 @@ class User extends Component {
         <FullColumn>
           <Papersheet title="Users of company CSBG">
             <Table>
-              <TableBody>
-                {this.renderData()}
-              </TableBody>
+              <TableBody>{this.renderData()}</TableBody>
             </Table>
             <Grid
               container
