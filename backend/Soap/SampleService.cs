b91e8bdf4ef3672f0ec487c498c62352e3458430
Michael Pham <phamminh1309@gmail.com>
2019-10-05 18:03:29 +0700

    add soap project

---
+++ backend/Soap/SampleService.cs
@@ -0,0 +1,51 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Server
+{
+	public class SampleService : ISampleService
+	{
+		public string Ping(string s)
+		{
+			Console.WriteLine("Exec ping method");
+			return s;
+		}
+
+		public ComplexModelResponse PingComplexModel(ComplexModelInput inputModel)
+		{
+			Console.WriteLine("Input data. IntProperty: {0}, StringProperty: {1}", inputModel.IntProperty, inputModel.StringProperty);
+
+            return new ComplexModelResponse
+			{
+				FloatProperty = float.MaxValue / 2,
+				StringProperty = inputModel.StringProperty,
+				ListProperty = inputModel.ListProperty,
+				DateTimeOffsetProperty = inputModel.DateTimeOffsetProperty
+			};
+		}
+
+		public void VoidMethod(out string s)
+		{
+			s = "Value from server";
+		}
+
+		public Task<int> AsyncMethod()
+		{
+			return Task.Run(() => 42);
+		}
+
+		public int? NullableMethod(bool? arg)
+		{
+			return null;
+		}
+
+		public void XmlMethod(XElement xml)
+		{
+			Console.WriteLine(xml.ToString());
+		}
+    }
+}
---
+++ backend/Soap/Startup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+using Models;
+using SoapCore;
+
+namespace Server
+{
+	public class Startup
+	{
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.TryAddSingleton<ISampleService, SampleService>();
+			services.AddMvc(x => x.EnableEndpointRouting = false);
+			services.AddSoapCore();
+		}
+
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+		{
+			app.UseSoapEndpoint<ISampleService>("/Service.svc", new BasicHttpBinding(), SoapSerializer.DataContractSerializer);
+			app.UseSoapEndpoint<ISampleService>("/Service.asmx", new BasicHttpBinding(), SoapSerializer.XmlSerializer);
+
+			app.UseMvc();
+		}
+	}
+}
