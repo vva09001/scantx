d795cf390c69f6c382e50bd28dee88eef2d27039
miracles-group <help@miracles.vn>
2019-11-01 16:05:18 +0700

    fix css table

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MaterialAdmin from './materialAdmin';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<MaterialAdmin />, document.getElementById('root'));
